b8e3b53b166a44995f7cbd7ecc9d8388a7045f7e
Ryan McQuen <Ryan.McQuen@surlatable.com>
2018-03-21 12:17:56 -0700

    Fix default only exports for reals.

--- async-wrap-exports.js
+++ async-wrap-exports.js
@@ -21,21 +21,33 @@ const transformer = (file, api, options) => {
 					/[A-Z]/.test(importName.slice(0, 1)) &&
 					path.node.specifiers.length === 1
 				) {
-					// TODO:
-					// We need to verify if we need to accomodate more
-					// than one specifier in the case of index.js files.
-					path.node.specifiers[0] = importName;
+					// If the export does:
+					//     export { default } from './AppBar'
+					// We want it to be:
+					//     export default AppBar;
+					// Otherwise it fails Material UI's styleguide/linting.
+					if (
+						path.node.specifiers[0].local.name ===
+						path.node.specifiers[0].exported.name
+					) {
+						j(path).replaceWith(`export default ${importName};`);
+					} else {
+						// TODO:
+						// We need to verify if we need to accomodate more
+						// than one specifier in the case of index.js files.
+						path.node.specifiers[0] = importName;
 
-					j(path).insertBefore(
-						[
-							`const ${importName} = asyncComponent({`,
-							`	resolve: () => import('${
-								path.node.source.value
-							}'),`,
-							`});`
-						].join('\n')
-					);
-					delete path.node.source;
+						j(path).insertBefore(
+							[
+								`const ${importName} = asyncComponent({`,
+								`	resolve: () => import('${
+									path.node.source.value
+								}'),`,
+								`});`
+							].join('\n')
+						);
+						delete path.node.source;
+					}
 				}
 			}
 			return path;
