2d79b02883c11d3fc297d247a15f3d676d951278
Ryan McQuen <Ryan.McQuen@surlatable.com>
2018-04-05 14:43:34 -0700

    Testing out index.es.js with comparison chart in sandbox.

--- sandbox/src/index.js
+++ sandbox/src/index.js
@@ -1,13 +1,41 @@
 import React from 'react';
 import { render } from 'react-dom';
-import { Button } from '@sur-la-table/slt-ui';
+import CompareCheckbox from './CompareCheckbox';
+import ComparisonTable from './ComparisonTable';
+import { Provider } from 'react-redux';
+import { createStore } from 'redux';
+// import { MuiThemeProvider, createMuiTheme } from '@sur-la-table/slt-ui';
+
+// import { purple, green } from "@sur-la-table/slt-ui/material-ui/src/colors";
+import reducers from './reducers';
+
+const sltStore = createStore(
+  reducers,
+  // Enable DevTools:
+  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__(),
+);
+
+const styles = {
+  fontFamily: 'sans-serif',
+  textAlign: 'center',
+};
+// const theme = createMuiTheme({
+//     palette: {
+//         primary: green,
+//         secondary: purple
+//     },
+//     status: {
+//         danger: "orange"
+//     }
+// });
 
 const App = () => (
-  <React.Fragment>
-    <Button color="primary" variant="raised">
-      Baz
-    </Button>
-  </React.Fragment>
+  <Provider store={sltStore}>
+    <div style={styles}>
+      <CompareCheckbox />
+      <ComparisonTable />
+    </div>
+  </Provider>
 );
 
 render(<App />, document.querySelector('.app'));
