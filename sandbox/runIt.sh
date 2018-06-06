e6902dd9280c9b6fcbf9b8800cf36a97646f7450
Ryan McQuen <Ryan.McQuen@surlatable.com>
2018-06-06 10:37:24 -0700

    More changes to sandbox.

--- sandbox/runIt.sh
+++ sandbox/runIt.sh
@@ -2,9 +2,9 @@
 
 #cp -r ../material-ui/packages/material-ui/src/ node_modules/@sur-la-table/slt-ui && \
 rm yarn.lock; \
+	cp -r ../material-ui/packages/material-ui ./mui && \
 	yarn install && \
 #	mkdir -p ./node_modules/@sur-la-table/slt-ui && \
 #	pwd && \
-	cp -r ../material-ui/packages/material-ui/src/ ./mui && \
 #	ln -sfv ../material-ui/packages/material-ui/src/ node_modules/@sur-la-table/slt-ui && \
 	yarn start
