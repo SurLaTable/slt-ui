7d4b4fce6e60482bfc0886c9303ae52bae355b84
Leonard Meagher <LeonardMeagher2@gmail.com>
2018-06-28 16:39:21 -0700

    Testing input label

--- src/index.js
+++ src/index.js
@@ -1,6 +1,5 @@
 export * from '@material-ui/core';
 export * from './ComparisonChart';
-export * from './SLTTest';
 
 if(process.env.NODE_ENV === 'development'){
   console.warn('Using development version of slt-ui');
