2cef4b2f7026fd70d9964e063a84381a1d50b452
Leonard Meagher <LeonardMeagher2@gmail.com>
2018-06-28 15:45:01 -0700

    add slide to comparison bar

--- src/ComparisonChart/ComparisonBar/ComparisonBar.js
+++ src/ComparisonChart/ComparisonBar/ComparisonBar.js
@@ -3,6 +3,7 @@ import { actionRemoveProduct } from '../actions/productComparisonActions';
 import { connect } from 'react-redux';
 
 import {Badge, BottomNavigation, BottomNavigationAction, Paper} from '@material-ui/core/';
+import Slide from '@material-ui/core/Slide';
 import { AddBox as AddBoxIcon, Cancel as CancelIcon } from '../icons';
 import ComparisonTable from '../ComparisonTable/ComparisonTable';
 
@@ -18,72 +19,74 @@ class ComparisonBar extends React.Component {
       .fill()
       .map((ignore, index) => (props.selection && props.selection[index]) || index);
 
-    return typeof selection[0] === 'object' ? (
-      <Paper
-        ref={ComparisonBarContainer => (this.ComparisonBarContainer = ComparisonBarContainer)}
-        style={{
-          backgroundColor: '#E4E4E4',
-          bottom: 0,
-          marginLeft: `${(global.innerWidth - global.innerWidth * 0.9) / 2}px`,
-          paddingBottom: '48px',
-          position: 'fixed',
-          width: '90%',
-          zIndex: '1300',
-        }}
-      >
-        <BottomNavigation showLabels style={{ backgroundColor: '#E4E4E4', marginTop: '10px' }}>
-          <ComparisonTable type="cutlery" />
+    return (
+      <Slide direction="up" in={typeof selection[0] === 'object'} mountOnEnter unmountOn>
+        <Paper
+          ref={ComparisonBarContainer => (this.ComparisonBarContainer = ComparisonBarContainer)}
+          elevation={4}
+          style={{
+            backgroundColor: '#E4E4E4',
+            bottom: 0,
+            marginLeft: `${(global.innerWidth - global.innerWidth * 0.9) / 2}px`,
+            paddingBottom: '48px',
+            position: 'fixed',
+            width: '90%',
+          }}
+        >
+          <BottomNavigation showLabels style={{ backgroundColor: '#E4E4E4', marginTop: '10px' }}>
+            <ComparisonTable type="cutlery" />
 
-          {selection &&
-            selection.map((product, index) => {
-              // Since the product data contains multiple skus,
-              // we just grab the first one available.
-              // We may decide later to change this behavior,
-              // with some kind of user selection tool.
-              const productData = product ? product[Object.keys(product)[0]] : index;
-              const productId = product.id || '';
+            {selection &&
+              selection.map((product, index) => {
+                // Since the product data contains multiple skus,
+                // we just grab the first one available.
+                // We may decide later to change this behavior,
+                // with some kind of user selection tool.
+                const productData = product ? product[Object.keys(product)[0]] : index;
+                const productId = product.id || '';
 
-              return (
-                <BottomNavigationAction
-                  key={index}
-                  label={
-                    productData
-                      ? `${productData['Web Brand']} ${productData.Collection}`
-                      : `Item #${index + 1}`
-                  }
-                  // Disable the ripple animation if the item box is empty:
-                  disableRipple={!productId.length}
-                  icon={
-                    <React.Fragment>
-                      {productId.length ? (
-                        <Badge
-                          data-product-id={productId}
-                          badgeContent={<CancelIcon />}
-                          onClick={(event, checked) => {
-                            props.dispatch(actionRemoveProduct(productId));
-                          }}
-                        >
-                          <img
-                            alt={`${productData['Web Brand']} ${productData.Collection}`}
-                            src={`https://www.surlatable.com/images/customers/c1079/${productId}/generated/${productId}_Default_1_200x200.jpg`}
-                            style={{
-                              border: '1px solid black',
-                              height: '50px',
-                              width: '50px',
+                return (
+                  <BottomNavigationAction
+                    key={index}
+                    label={
+                      productData
+                        ? `${productData['Web Brand']} ${productData.Collection}`
+                        : `Item #${index + 1}`
+                    }
+                    // Disable the ripple animation if the item box is empty:
+                    disableRipple={!productId.length}
+                    icon={
+                      <React.Fragment>
+                        {productId.length ? (
+                          <Badge
+                            data-product-id={productId}
+                            badgeContent={<CancelIcon />}
+                            onClick={(event, checked) => {
+                              props.dispatch(actionRemoveProduct(productId));
                             }}
-                          />
-                        </Badge>
-                      ) : (
-                        <AddBoxIcon style={{ fontSize: 50 }} />
-                      )}
-                    </React.Fragment>
-                  }
-                />
-              );
-            })}
-        </BottomNavigation>
-      </Paper>
-    ) : null;
+                          >
+                            <img
+                              alt={`${productData['Web Brand']} ${productData.Collection}`}
+                              src={`https://www.surlatable.com/images/customers/c1079/${productId}/generated/${productId}_Default_1_200x200.jpg`}
+                              style={{
+                                border: '1px solid black',
+                                height: '50px',
+                                width: '50px',
+                              }}
+                            />
+                          </Badge>
+                        ) : (
+                          <AddBoxIcon style={{ fontSize: 50 }} />
+                        )}
+                      </React.Fragment>
+                    }
+                  />
+                );
+              })}
+          </BottomNavigation>
+        </Paper>
+      </Slide>
+    );
   }
 }
 
