0b63e06d14b42cc5f91739dff90a09ed747b1e93
Ryan P. C. McQuen <ryanpcmcquen@member.fsf.org>
2018-07-01 00:53:16 -0400

    Fix CodeSandbox functionality.
    Signed-off-by: Ryan P. C. McQuen <ryanpcmcquen@member.fsf.org>

---
+++ CodeSandbox/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { render } from 'react-dom';
+import { ComparisonCheckbox, StoreProvider } from '../src/index.js';
+
+render(
+	<React.Fragment>
+		<StoreProvider>
+			<ComparisonCheckbox product="PRO-2403251" />
+		</StoreProvider>
+	</React.Fragment>,
+	document.querySelector('#root')
+);
