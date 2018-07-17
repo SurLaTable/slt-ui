177b2fce043b84bf4cf4e2cd698587c0a48c7c4f
Ryan P. C. McQuen <ryanpcmcquen@member.fsf.org>
2018-07-16 17:24:00 -0700

    Reworking table structure for fixed header.

--- src/ComparisonChart/ComparisonTable/ComparisonTable.js
+++ src/ComparisonChart/ComparisonTable/ComparisonTable.js
@@ -11,6 +11,7 @@ import MuiThemeProvider from '@material-ui/core/styles/MuiThemeProvider';
 import Badge from '@material-ui/core/Badge';
 import Button from '@material-ui/core/Button';
 import Dialog from '@material-ui/core/Dialog';
+import DialogTitle from '@material-ui/core/DialogTitle';
 import Slide from '@material-ui/core/Slide';
 import Table from '@material-ui/core/Table';
 import TableBody from '@material-ui/core/TableBody';
@@ -185,8 +186,8 @@ class ComparisonTable extends React.Component {
 								height: `${window.innerHeight}px`,
 								maxHeight: `${window.innerHeight}px`,
 								maxWidth: `${window.innerWidth * 0.95}px`,
-								width: `${window.innerWidth * 0.95}px`,
-								overflowX: 'hidden'
+								width: `${window.innerWidth * 0.95}px`
+								// overflowX: 'hidden'
 							}
 						}}
 						maxWidth={false}
@@ -197,284 +198,311 @@ class ComparisonTable extends React.Component {
 						transitionDuration={600}
 					>
 						<Table>
-							<TableHead style={{ position: 'sticky', top: 0 }}>
-								<TableRow
-									style={{
-										backgroundColor: '#E4E4E4'
-									}}
-								>
-									<TableCell style={tableCellStyles}>
-										<Button
-											onClick={this.handleClose.bind(
-												this
-											)}
-										>
-											<PlayForWorkIcon />Hide chart
-										</Button>
-									</TableCell>
-									{props.selection.map((product, index) => {
-										const first =
-											product[Object.keys(product)[0]];
-										return first ? (
-											<TableCell
-												key={index}
-												style={tableCellStyles}
+							<DialogTitle>
+								<TableHead>
+									<TableRow
+										style={{
+											backgroundColor: '#E4E4E4'
+										}}
+									>
+										<TableCell style={tableCellStyles}>
+											<Button
+												onClick={this.handleClose.bind(
+													this
+												)}
 											>
-												<Badge
-													data-product-id={product.id}
-													badgeContent={
-														<CancelIcon />
-													}
-													style={badgeStyles}
-													onClick={(
-														event,
-														checked
-													) => {
-														props.dispatch(
-															actionRemoveProduct(
+												<PlayForWorkIcon />Hide chart
+											</Button>
+										</TableCell>
+										{props.selection.map(
+											(product, index) => {
+												const first =
+													product[
+														Object.keys(product)[0]
+													];
+												return first ? (
+													<TableCell
+														key={index}
+														style={tableCellStyles}
+													>
+														<Badge
+															data-product-id={
 																product.id
-															)
-														);
-													}}
-												>
-													<img
-														alt={`${
-															first['Web Brand']
-														} ${first.Collection}`}
-														style={imageStyles}
-														src={`https://www.surlatable.com/images/customers/c1079/${
-															product.id
-														}/generated/${
-															product.id
-														}_Default_1_200x200.jpg`}
-													/>
-												</Badge>
-												{`${first['Web Brand']} ${
-													first.Collection
-												}`}
-											</TableCell>
-										) : null;
-									})}
-								</TableRow>
-							</TableHead>
-
-							{sections.map((section, index) => {
-								return (
-									<React.Fragment key={index}>
-										<TableHead>
-											<TableRow
-												style={{
-													backgroundColor: '#111111',
-													height: '36px'
-												}}
-											>
-												{/*
-                        This Array().fill() has to exist to allow the column to
-                        extend one beyond the selection length (for the sake of
-                        the first column).
-                      */}
-												{props.selection &&
-													Array(
-														props.selection.length +
-															1
-													)
-														.fill()
-														.map(
-															(ignore, index) =>
-																index ? (
-																	<TableCell
-																		key={
-																			index
-																		}
-																		style={
-																			tableCellStyles
-																		}
-																	/>
-																) : (
-																	<TableCell
-																		key={
-																			index
-																		}
-																		style={
-																			// Extend an empty object with our default styles:
-																			Object.assign(
-																				{},
-																				tableCellStyles,
-																				{
-																					color:
-																						'#ffffff',
-																					fontWeight: 900
-																				}
-																			)
-																		}
-																	>
-																		{
-																			section
-																		}
-																	</TableCell>
-																)
-														)}
-											</TableRow>
-										</TableHead>
-										<TableBody>
-											{attributes[section].map(
-												(attribute, index) => {
-													let format = 'default';
-													// Determine if the property is an object or a string.
-													// We don't need to use strings for our later processing.
-													if (
-														typeof attribute !==
-														'string'
-													) {
-														format =
-															attribute.format;
-														attribute =
-															attribute.name;
-													}
-													return (
-														<TableRow
-															key={index}
-															style={{
-																// This alternates the color of every other row:
-																backgroundColor:
-																	(index ===
-																		1 ||
-																		index %
-																			2 !==
-																			0) &&
-																	'#eeeeee'
+															}
+															badgeContent={
+																<CancelIcon />
+															}
+															style={badgeStyles}
+															onClick={(
+																event,
+																checked
+															) => {
+																props.dispatch(
+																	actionRemoveProduct(
+																		product.id
+																	)
+																);
 															}}
 														>
-															<TableCell
-																key={index}
-																style={Object.assign(
-																	{},
-																	tableCellStyles,
-																	{
-																		fontWeight: 900
-																	}
-																)}
-															>
-																{attribute}
-															</TableCell>
-															{props.selection.map(
+															<img
+																alt={`${
+																	first[
+																		'Web Brand'
+																	]
+																} ${
+																	first.Collection
+																}`}
+																style={
+																	imageStyles
+																}
+																src={`https://www.surlatable.com/images/customers/c1079/${
+																	product.id
+																}/generated/${
+																	product.id
+																}_Default_1_200x200.jpg`}
+															/>
+														</Badge>
+														{`${
+															first['Web Brand']
+														} ${first.Collection}`}
+													</TableCell>
+												) : null;
+											}
+										)}
+									</TableRow>
+								</TableHead>
+							</DialogTitle>
+						</Table>
+						<Table>
+							<TableBody
+								style={{
+									width: `${window.innerWidth * 0.93}px`,
+									height: `${window.innerHeight}px`,
+									overflowY: 'scroll'
+								}}
+							>
+								{sections.map((section, index) => {
+									return (
+										<React.Fragment key={index}>
+											<TableHead>
+												<TableRow
+													style={{
+														backgroundColor:
+															'#111111',
+														height: '36px'
+													}}
+												>
+													{
+														// This Array().fill() has to exist to allow the column to
+														// extend one beyond the selection length (for the sake of
+														// the first column).
+													}
+													{props.selection &&
+														Array(
+															props.selection
+																.length + 1
+														)
+															.fill()
+															.map(
 																(
-																	product,
+																	ignore,
 																	index
-																) => {
-																	let cellData = [];
-
-																	// At the end of processing the loop,
-																	// we store the last value, that
-																	// way we can check for duplicates.
-																	let lastValue;
-																	for (let sku in product) {
-																		if (
-																			product[
-																				sku
-																			][
-																				attribute
-																			] !==
-																			lastValue
-																		) {
-																			if (
-																				format ===
-																				'html'
-																			) {
-																				cellData.push(
-																					this.handleFormat(
-																						product[
-																							sku
-																						][
-																							attribute
-																						],
-																						format
-																					)
-																				);
-																			} else if (
-																				cellData.indexOf(
-																					product[
-																						sku
-																					][
-																						attribute
-																					]
-																				) ===
-																				-1
-																			) {
-																				cellData.push(
-																					this.handleFormat(
-																						product[
-																							sku
-																						][
-																							attribute
-																						],
-																						format
-																					)
-																				);
-																			}
-																		}
-
-																		lastValue =
-																			product[
-																				sku
-																			][
-																				attribute
-																			];
-																	}
-																	if (
-																		format ===
-																		'default'
-																	) {
-																		const delimiter =
-																			', ';
-																		cellData = cellData.join(
-																			delimiter
-																		);
-																		// Since there will always be
-																		// an extraneous comma and
-																		// space at the end after this
-																		// processing, we cut it off here.
-																		if (
-																			cellData.slice(
-																				-2
-																			) ===
-																			delimiter
-																		) {
-																			cellData = cellData.slice(
-																				0,
-																				-2
-																			);
-																		}
-																	}
-																	return (
+																) =>
+																	index ? (
 																		<TableCell
 																			key={
 																				index
 																			}
-																			style={Object.assign(
-																				{},
-																				tableCellStyles,
-																				{
-																					textAlign:
-																						'center'
-																				}
-																			)}
+																			style={
+																				tableCellStyles
+																			}
+																		/>
+																	) : (
+																		<TableCell
+																			key={
+																				index
+																			}
+																			style={
+																				// Extend an empty object with our default styles:
+																				Object.assign(
+																					{},
+																					tableCellStyles,
+																					{
+																						color:
+																							'#ffffff',
+																						fontWeight: 900
+																					}
+																				)
+																			}
 																		>
 																			{
-																				cellData
+																				section
 																			}
 																		</TableCell>
-																	);
-																}
+																	)
 															)}
-														</TableRow>
-													);
-												}
-											)}
-										</TableBody>
-									</React.Fragment>
-								);
-							})}
+												</TableRow>
+											</TableHead>
+											<TableBody>
+												{attributes[section].map(
+													(attribute, index) => {
+														let format = 'default';
+														// Determine if the property is an object or a string.
+														// We don't need to use strings for our later processing.
+														if (
+															typeof attribute !==
+															'string'
+														) {
+															format =
+																attribute.format;
+															attribute =
+																attribute.name;
+														}
+														return (
+															<TableRow
+																key={index}
+																style={{
+																	// This alternates the color of every other row:
+																	backgroundColor:
+																		(index ===
+																			1 ||
+																			index %
+																				2 !==
+																				0) &&
+																		'#eeeeee'
+																}}
+															>
+																<TableCell
+																	key={index}
+																	style={Object.assign(
+																		{},
+																		tableCellStyles,
+																		{
+																			fontWeight: 900
+																		}
+																	)}
+																>
+																	{attribute}
+																</TableCell>
+																{props.selection.map(
+																	(
+																		product,
+																		index
+																	) => {
+																		let cellData = [];
+
+																		// At the end of processing the loop,
+																		// we store the last value, that
+																		// way we can check for duplicates.
+																		let lastValue;
+																		for (let sku in product) {
+																			if (
+																				product[
+																					sku
+																				][
+																					attribute
+																				] !==
+																				lastValue
+																			) {
+																				if (
+																					format ===
+																					'html'
+																				) {
+																					cellData.push(
+																						this.handleFormat(
+																							product[
+																								sku
+																							][
+																								attribute
+																							],
+																							format
+																						)
+																					);
+																				} else if (
+																					cellData.indexOf(
+																						product[
+																							sku
+																						][
+																							attribute
+																						]
+																					) ===
+																					-1
+																				) {
+																					cellData.push(
+																						this.handleFormat(
+																							product[
+																								sku
+																							][
+																								attribute
+																							],
+																							format
+																						)
+																					);
+																				}
+																			}
+
+																			lastValue =
+																				product[
+																					sku
+																				][
+																					attribute
+																				];
+																		}
+																		if (
+																			format ===
+																			'default'
+																		) {
+																			const delimiter =
+																				', ';
+																			cellData = cellData.join(
+																				delimiter
+																			);
+																			// Since there will always be
+																			// an extraneous comma and
+																			// space at the end after this
+																			// processing, we cut it off here.
+																			if (
+																				cellData.slice(
+																					-2
+																				) ===
+																				delimiter
+																			) {
+																				cellData = cellData.slice(
+																					0,
+																					-2
+																				);
+																			}
+																		}
+																		return (
+																			<TableCell
+																				key={
+																					index
+																				}
+																				style={Object.assign(
+																					{},
+																					tableCellStyles,
+																					{
+																						textAlign:
+																							'center'
+																					}
+																				)}
+																			>
+																				{
+																					cellData
+																				}
+																			</TableCell>
+																		);
+																	}
+																)}
+															</TableRow>
+														);
+													}
+												)}
+											</TableBody>
+										</React.Fragment>
+									);
+								})}
+							</TableBody>
 						</Table>
 					</Dialog>
 				</div>
