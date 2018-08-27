b82492660625b6321e2188d42ec60b3caf13dd7a
Ryan P. C. McQuen <ryanpcmcquen@member.fsf.org>
2018-08-27 14:21:33 -0700

    Specify 'assetsDir' for static docs.

--- builder/docs/styleguide.config.js
+++ builder/docs/styleguide.config.js
@@ -19,7 +19,7 @@ function getDemos() {
 }
 
 const config = {
-	assetsDir: path.resolve('./build/'),
+	assetsDir: path.resolve('./builder/docs/assets'),
 	defaultExample: path.resolve(__dirname, 'DefaultExample.md'),
 	pagePerSection: true,
 
